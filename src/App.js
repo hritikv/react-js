5f6840e4f851f6ab46729d6b9128be3faa3ce681
hritik vishwakarma <hritik5269@gmail.com>
2022-11-29 20:00:29 +0530

    new react project progressing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Expenses from "./components/EXPENSES/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const DUMMY_EXPENSES = [
+  {
+    id: "el1",
+    title: "Toilet-Paper",
+    amount: 200,
+    date: new Date(2022, 11, 24),
+  },
+  {
+    id: "el2",
+    title: "News-Paper",
+    amount: 250,
+    date: new Date(2021, 1, 4),
+  },
+  {
+    id: "el3",
+    title: "TV",
+    amount: 9200,
+    date: new Date(2020, 10, 20),
+  },
+  {
+    id: "el4",
+    title: "Car Insurance",
+    amount: 2000,
+    date: new Date(2022, 5, 6),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = expense=> {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
+  };
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2',{},"let's get started"),
+  //   React.createElement(Expenses,{items:expenses})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
